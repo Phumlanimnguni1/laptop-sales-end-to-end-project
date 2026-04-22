53345bc68ff89f0c7be66d27e2977c1295adeef3
phumlanimnguni1 <prosperproper700@gmail.com>
2026-04-22 18:50:12 +0200

    feat: add stored procedures for all tables

--- dataProcessing/01_create_all_tables.sql
+++ dataProcessing/01_create_all_tables.sql
@@ -141,14 +141,14 @@ CREATE TABLE [computer_std].[dbo].[fact_pc_sales](
 [ChannelID] INT,
 [PriorityID] INT,
 [DateID] INT,
-[Cost_Price] [int] NOT NULL, 
-[Sale_Price] [int] NOT NULL,
-[Discount_Amount] [int] NOT NULL,
-[Finance_Amount] [int] NOT NULL,
-[Credit_Score] [int] NOT NULL,
-[Cost_of_Repairs] [int] NOT NULL, 
-[Total_Sales_per_Employee] [int] NOT NULL,
-[PC_Market_Price] [int] NOT NULL, 
+[Cost_Price] decimal(10,2) NOT NULL, 
+[Sale_Price] decimal(10,2) NOT NULL,
+[Discount_Amount] decimal(10,2) NOT NULL,
+[Finance_Amount] decimal(10,2) NOT NULL,
+[Credit_Score] decimal(10,2) NOT NULL,
+[Cost_of_Repairs] decimal(10,2) NOT NULL, 
+[Total_Sales_per_Employee] decimal(10,2) NOT NULL,
+[PC_Market_Price] decimal(10,2) NOT NULL, 
 CONSTRAINT fk_LocationID
            foreign key (LocationID)
            references [computer_std].[dbo].[dim_location] (LocationID),
@@ -180,25 +180,75 @@ CONSTRAINT fk_CustomerID
 
 --Insert values into table from raw data
 INSERT INTO [computer_std].[dbo].[fact_pc_sales](
-            [Cost_Price],
-            [Sale_Price],
-            [Discount_Amount],
-            [Finance_Amount],
-            [Credit_Score],
-            [Cost_of_Repairs],
-            [Total_Sales_per_Employee],
-            [PC_Market_Price]
+    [LocationID], 
+    [StoreID], 
+    [PcspecID], 
+    [CustomerID], 
+    [SalespersonID], 
+    [PaymentID], 
+    [ChannelID], 
+    [PriorityID], 
+    [DateID],
+    [Cost_Price], 
+    [Sale_Price], 
+    [Discount_Amount], 
+    [Finance_Amount], 
+    [Credit_Score], 
+    [Cost_of_Repairs], 
+    [Total_Sales_per_Employee], 
+    [PC_Market_Price]
 )
-SELECT DISTINCT
-               [Cost_Price],
-               [Sale_Price],
-               [Discount_Amount],
-               [Finance_Amount],
-               [Credit_Score],
-               [Cost_of_Repairs],
-               [Total_Sales_per_Employee],
-               [PC_Market_Price]
-FROM [computer_std].[dbo].[raw_pc_data]
+SELECT 
+    loc.LocationID,
+    st.StoreID,
+    pc.PcspecID,
+    cus.CustomerID,
+    sp.SalespersonID,
+    pay.PaymentID,
+    ch.ChannelID,
+    pr.PriorityID,
+    d.DateID,
+    raw.[Cost_Price], 
+    raw.[Sale_Price], 
+    raw.[Discount_Amount], 
+    raw.[Finance_Amount], 
+    raw.[Credit_Score], 
+    raw.[Cost_of_Repairs], 
+    raw.[Total_Sales_per_Employee], 
+    raw.[PC_Market_Price]
+FROM [computer_std].[dbo].[raw_pc_data] raw
+-- You must join every dimension to match the raw text to the new ID
+LEFT JOIN [computer_std].[dbo].[dim_location] loc 
+    ON raw.Continent = loc.Continent 
+    AND raw.Country_or_State = loc.Country_or_State 
+    AND raw.Province_or_City = loc.Province_or_City
+LEFT JOIN [computer_std].[dbo].[dim_store] st 
+    ON raw.Shop_Name = st.Shop_Name 
+    AND raw.Shop_Age = st.Shop_Age
+LEFT JOIN [computer_std].[dbo].[dim_pcspec] pc 
+    ON raw.PC_Make = pc.PC_Make 
+    AND raw.PC_Model = pc.PC_Model 
+    AND raw.Storage_Type = pc.Storage_Type 
+    AND raw.RAM = pc.RAM 
+    AND raw.Storage_Capacity = pc.Storage_Capacity
+LEFT JOIN [computer_std].[dbo].[dim_customer] cus 
+    ON raw.Customer_Name = cus.Customer_Name 
+    AND raw.Customer_Surname = cus.Customer_Surname 
+    AND raw.Customer_Contact_Number = cus.Customer_Contact_Number 
+    AND raw.Customer_Email_Address = cus.Customer_Email_Address
+LEFT JOIN [computer_std].[dbo].[dim_salesperson] sp 
+    ON raw.Sales_Person_Name = sp.Sales_Person_Name 
+    AND raw.Sales_Person_Department = sp.Sales_Person_Department
+LEFT JOIN [computer_std].[dbo].[dim_payment] pay 
+    ON raw.Payment_Method = pay.Payment_Method
+LEFT JOIN [computer_std].[dbo].[dim_channel] ch 
+    ON raw.Channel = ch.Channel
+LEFT JOIN [computer_std].[dbo].[dim_priority] pr 
+    ON raw.Priority = pr.Priority
+LEFT JOIN [computer_std].[dbo].[dim_date] d 
+    ON raw.Purchase_Date = d.Purchase_Date 
+    AND raw.Ship_Date = d.Ship_Date;
+
 
 --Show all the sales
 SELECT *
---
+++ dataProcessing/02_stored_procedures.sql
@@ -0,0 +1,212 @@
+-- STORED PROCEDURES FOR DATA WAREHOUSE LOAD
+
+USE [computer_std];
+GO
+--A. STORED PROCEDURE FOR DIMENSION TABLES
+
+-- 1. Load dim_location
+CREATE OR ALTER PROCEDURE [dbo].[Load_Dim_Location]
+AS
+BEGIN
+    INSERT INTO [dbo].[dim_location] (Continent, Country_or_State, Province_or_City)
+    SELECT DISTINCT Continent, Country_or_State, Province_or_City 
+    FROM [dbo].[raw_pc_data]
+    WHERE Continent IS NOT NULL; 
+END;
+GO
+
+--2. Load dim_customer
+CREATE OR ALTER PROCEDURE [dbo].[Load_Dim_Customer]
+AS
+BEGIN
+    INSERT INTO [dbo].[dim_customer] (Customer_Name, Customer_Surname, Customer_Contact_Number, Customer_Email_Address)
+    SELECT DISTINCT Customer_Name, Customer_Surname, Customer_Contact_Number, Customer_Email_Address 
+    FROM [dbo].[raw_pc_data]
+    WHERE Customer_Name IS NOT NULL;
+END;
+GO
+
+--3. Load dim_payment
+CREATE OR ALTER PROCEDURE [dbo].[Load_Dim_Payment]
+AS
+BEGIN
+    INSERT INTO [dbo].[dim_payment] (Payment_Method)
+    SELECT DISTINCT Payment_Method 
+    FROM [dbo].[raw_pc_data]
+    WHERE Payment_Method IS NOT NULL;
+END;
+GO
+
+--4. Load dim_pcspec
+CREATE OR ALTER PROCEDURE [dbo].[Load_Dim_PCSpec]
+AS
+BEGIN
+    INSERT INTO [dbo].[dim_pcspec] (PC_Make, PC_Model, Storage_Type, RAM, Storage_Capacity)
+    SELECT DISTINCT PC_Make, PC_Model, Storage_Type, RAM, Storage_Capacity 
+    FROM [dbo].[raw_pc_data]
+    WHERE PC_Make IS NOT NULL;
+END;
+GO
+
+--5. Load dim_salesperson
+CREATE OR ALTER PROCEDURE [dbo].[Load_Dim_Salesperson]
+AS
+BEGIN
+    INSERT INTO [dbo].[dim_salesperson] (Sales_Person_Name, Sales_Person_Department)
+    SELECT DISTINCT Sales_Person_Name, Sales_Person_Department 
+    FROM [dbo].[raw_pc_data]
+    WHERE Sales_Person_Name IS NOT NULL;
+END;
+GO
+
+--6. Load dim_priority
+CREATE OR ALTER PROCEDURE [dbo].[Load_Dim_Priority]
+AS
+BEGIN
+    INSERT INTO [dbo].[dim_priority] (Priority)
+    SELECT DISTINCT Priority 
+    FROM [dbo].[raw_pc_data]
+    WHERE Priority IS NOT NULL;
+END;
+GO
+
+--7. Load dim_channel
+CREATE OR ALTER PROCEDURE [dbo].[Load_Dim_Channel]
+AS
+BEGIN
+    INSERT INTO [dbo].[dim_channel] (Channel)
+    SELECT DISTINCT Channel 
+    FROM [dbo].[raw_pc_data]
+    WHERE Channel IS NOT NULL;
+END;
+GO
+
+--8. Load dim_date
+CREATE OR ALTER PROCEDURE [dbo].[Load_Dim_Date]
+AS
+BEGIN
+    INSERT INTO [dbo].[dim_date] (Purchase_Date, Ship_Date)
+    SELECT DISTINCT Purchase_Date, Ship_Date 
+    FROM [dbo].[raw_pc_data]
+    WHERE Purchase_Date IS NOT NULL;
+END;
+GO
+
+--9. Load dim_store
+CREATE OR ALTER PROCEDURE [dbo].[Load_Dim_Store]
+AS
+BEGIN
+    INSERT INTO [dbo].[dim_store] (Shop_Name, Shop_Age)
+    SELECT DISTINCT Shop_Name, Shop_Age 
+    FROM [dbo].[raw_pc_data]
+    WHERE Shop_Name IS NOT NULL;
+END;
+GO
+
+
+-- B. STORED PROCEDURE FOR FACT TABLE
+CREATE OR ALTER PROCEDURE [dbo].[Load_Fact_PC_Sales]
+AS
+BEGIN
+    INSERT INTO [dbo].[fact_pc_sales](
+        [LocationID], 
+        [StoreID], 
+        [PcspecID], 
+        [CustomerID], 
+        [SalespersonID], 
+        [PaymentID], 
+        [ChannelID], 
+        [PriorityID], 
+        [DateID],
+        [Cost_Price], 
+        [Sale_Price], 
+        [Discount_Amount], 
+        [Finance_Amount], 
+        [Credit_Score], 
+        [Cost_of_Repairs], 
+        [Total_Sales_per_Employee], 
+        [PC_Market_Price]
+    )
+    SELECT 
+        loc.LocationID,
+        st.StoreID,
+        pc.PcspecID,
+        cus.CustomerID,
+        sp.SalespersonID,
+        pay.PaymentID,
+        ch.ChannelID,
+        pr.PriorityID,
+        d.DateID,
+        raw.[Cost_Price], 
+        raw.[Sale_Price], 
+        raw.[Discount_Amount], 
+        raw.[Finance_Amount], 
+        raw.[Credit_Score], 
+        raw.[Cost_of_Repairs], 
+        raw.[Total_Sales_per_Employee], 
+        raw.[PC_Market_Price]
+    FROM [dbo].[raw_pc_data] raw
+    LEFT JOIN [dbo].[dim_location] loc 
+        ON raw.Continent = loc.Continent 
+        AND raw.Country_or_State = loc.Country_or_State 
+        AND raw.Province_or_City = loc.Province_or_City
+    LEFT JOIN [dbo].[dim_store] st 
+        ON raw.Shop_Name = st.Shop_Name 
+        AND raw.Shop_Age = st.Shop_Age
+    LEFT JOIN [dbo].[dim_pcspec] pc 
+        ON raw.PC_Make = pc.PC_Make 
+        AND raw.PC_Model = pc.PC_Model 
+        AND raw.Storage_Type = pc.Storage_Type 
+        AND raw.RAM = pc.RAM 
+        AND raw.Storage_Capacity = pc.Storage_Capacity
+    LEFT JOIN [dbo].[dim_customer] cus 
+        ON raw.Customer_Name = cus.Customer_Name 
+        AND raw.Customer_Surname = cus.Customer_Surname 
+        AND raw.Customer_Contact_Number = cus.Customer_Contact_Number 
+        AND raw.Customer_Email_Address = cus.Customer_Email_Address
+    LEFT JOIN [dbo].[dim_salesperson] sp 
+        ON raw.Sales_Person_Name = sp.Sales_Person_Name 
+        AND raw.Sales_Person_Department = sp.Sales_Person_Department
+    LEFT JOIN [dbo].[dim_payment] pay 
+        ON raw.Payment_Method = pay.Payment_Method
+    LEFT JOIN [dbo].[dim_channel] ch 
+        ON raw.Channel = ch.Channel
+    LEFT JOIN [dbo].[dim_priority] pr 
+        ON raw.Priority = pr.Priority
+    LEFT JOIN [dbo].[dim_date] d 
+        ON raw.Purchase_Date = d.Purchase_Date 
+        AND raw.Ship_Date = d.Ship_Date;
+END;
+GO
+-- EXECUTION BLOCK
+/*
+USE [computer_std]
+GO
+A. Delete data inside the tables
+-- 1. Delete the Fact table data first to release the foreign key locks
+DELETE FROM [dbo].[fact_pc_sales];
+
+-- 2. Delete the Dimension tables data
+DELETE FROM [dbo].[dim_location];
+DELETE FROM [dbo].[dim_customer];
+DELETE FROM [dbo].[dim_payment];
+DELETE FROM [dbo].[dim_pcspec];
+DELETE FROM [dbo].[dim_salesperson];
+DELETE FROM [dbo].[dim_priority];
+DELETE FROM [dbo].[dim_channel];
+DELETE FROM [dbo].[dim_date];
+DELETE FROM [dbo].[dim_store];
+
+B. Load data inside the tables
+
+EXEC [dbo].[Load_Dim_Location];
+EXEC [dbo].[Load_Dim_Customer];
+EXEC [dbo].[Load_Dim_Payment];
+EXEC [dbo].[Load_Dim_PCSpec];
+EXEC [dbo].[Load_Dim_Salesperson];
+EXEC [dbo].[Load_Dim_Priority];
+EXEC [dbo].[Load_Dim_Channel];
+EXEC [dbo].[Load_Dim_Date];
+EXEC [dbo].[Load_Dim_Store];
+EXEC [dbo].[Load_Fact_PC_Sales];
+*/
